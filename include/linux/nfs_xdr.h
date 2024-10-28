7db20385680c6b436461787ee7002e46383eef31
Rick Macklem <rmacklem@uoguelph.ca>
2024-10-28 12:25:17 -0700

    Add include entries for the POSIX draft ACL attributes
    Signed-off-by: Rick Macklem <rmacklem@uoguelph.ca>

--- include/linux/nfs_xdr.h
+++ include/linux/nfs_xdr.h
@@ -1616,6 +1616,46 @@ struct nfs42_removexattrres {
 	struct nfs4_change_info		cinfo;
 };
 
+struct nfs42_getposixaclargs {
+	struct nfs4_sequence_args 	seq_args;
+	struct nfs_fh *			fh;
+	int				mask;
+	struct page **			pages;
+};
+
+struct nfs42_getposixaclres {
+	struct nfs4_sequence_res	seq_res;
+	const struct nfs_server *	server;
+	int				mask;
+	unsigned int			acl_access_count;
+	unsigned int			acl_default_count;
+	struct posix_acl *		acl_access;
+	struct posix_acl *		acl_default;
+};
+
+struct nfs42_setposixaclargs {
+	struct nfs4_sequence_args 	seq_args;
+	struct nfs_fh *			fh;
+	const struct nfs_server *	server;
+	struct inode *			inode;
+	int				mask;
+	struct posix_acl *		acl_access;
+	struct posix_acl *		acl_default;
+	size_t				len;
+	struct page **			pages;
+};
+
+struct nfs42_setposixaclres {
+	struct nfs4_sequence_res	seq_res;
+	const struct nfs_server *	server;
+};
+
+#define	NFS4_ACL_INLINE_BUFSIZE	((2*(1+3*4+35*(NFS_ACL_MAX_ENTRIES_INLINE-4)))	\
+				 << 2)
+#define NFS4_ACL_MAXPAGES	((2*(4+12*NFS_ACL_MAX_ENTRIES+		\
+				 IDMAP_NAMESZ*(NFS_ACL_MAX_ENTRIES-4))+	\
+				 PAGE_SIZE - 1) >> PAGE_SHIFT)
+
 #endif /* CONFIG_NFS_V4_2 */
 
 struct nfs_page;
@@ -1755,6 +1795,15 @@ struct nfs_renamedata {
 	void (*complete)(struct rpc_task *, struct nfs_renamedata *);
 	long timeout;
 	bool cancelled;
+};
+
+struct nfs_xdr_putpage_desc {
+	struct page	**pages;
+	void		*p;
+	void		*endp;
+	size_t		npages;
+	size_t		page_pos;
+	size_t		max_npages;
 };
 
 struct nfs_access_entry;
